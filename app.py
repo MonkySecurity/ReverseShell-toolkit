77623110134999484cdb16b3badbcc76e256246a
MonkeySecurity <bahsmonkey@protonmail.com>
2017-08-03 22:48:57 +0200

    Beta 1.1

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import render_template, Flask, request
-
+import re
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
+	acceptedLangage=["bash","netcat","perl","php","python","ruby"]
 	ip=""
 	port=""
 	langage=""
@@ -16,6 +17,14 @@ def index():
 		print ip
 		print port
 		print langage
+		if ip != "" and port != "" and langage != "": 
+			if langage in acceptedLangage:
+				file=open("revshell/"+langage,"r")
+				for f in file:
+					f= f.replace("[ip]",ip)
+					f = f.replace("[port]",port)
+					print f
+				file.close()
 		return render_template('index.html')
 	else:
 		return render_template('index.html')
