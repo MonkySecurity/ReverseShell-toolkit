1aa4cec4172ff50628062271f8eee7e612cf30e7
MonkeySecurity <bahsmonkey@protonmail.com>
2017-08-07 23:50:24 +0200

    Version 1

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # ReverseShell-toolkit
 
 ## This is a web based reverse shell generator
+
+- ### Requierments
+
+	- python 2.7 or python 3
+	- python-pip
+	- python-setuptools
+	- flask
+	
+	 
+- ### Installation
+
+	- run "sudo apt-get install python2.7"
+	- run "sudo apt-get install python-pip"
+	- run "sudo apt-get install python-setuptools"
+	- run "sudo pip install flask"
+
+- ### Run
+ 	- Go in the Revershell toolkit folder
+ 	- run : "export FLASK_APP=app.py"
+ 	- run: "flask run"
+ 	- Go to http://localhost:5000/ on your browser
+ 	
+ Thats all
+ 
+! [ Screenshot ](screenshot/screenshot.png)
--- app.py
+++ app.py
@@ -6,25 +6,37 @@ app = Flask(__name__)
 def index():
 	acceptedLangage=["bash","netcat","perl","php","python","ruby"]
 	ip=""
+	ipregex= "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
 	port=""
+	portregex ="^([1-9][0-9]{0,3}|9000)$"
 	langage=""
-	payload=""
-	alert=""
+	server="nc -lvp "
 	if request.method == "POST":
 		ip=request.form['ip']
 		port=request.form['port']
 		langage=request.form['lan']
-		print ip
-		print port
-		print langage
-		if ip != "" and port != "" and langage != "": 
+		if re.match(ipregex,ip) and re.match(portregex,port) and langage in acceptedLangage: 
 			if langage in acceptedLangage:
 				file=open("revshell/"+langage,"r")
 				for f in file:
 					f= f.replace("[ip]",ip)
 					f = f.replace("[port]",port)
-					print f
 				file.close()
-		return render_template('index.html')
+				server = server + port
+			return render_template('index.html',payload=f,server=server,errors="")
+		return render_template('index.html',payload="",server="",errors=error(ip,port,langage))
 	else:
 		return render_template('index.html')
+
+def error(ip,port,langage):
+	error = []
+	ipregex= "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+	portregex ="^([1-9][0-9]{0,3}|9000)$"
+	acceptedLangage=["bash","netcat","perl","php","python","ruby"]
+	if not re.match(ipregex,ip):
+		error.append("enter a valid IP address")
+	if not re.match(portregex,port):
+		error.append("enter a valid port number [1-9999]")
+	if not langage in acceptedLangage:
+		error.append("use an accepted langage")
+	return error
